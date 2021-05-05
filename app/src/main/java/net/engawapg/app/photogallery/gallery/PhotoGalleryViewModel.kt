30760f92df1899b75d60749754776ce4adc4403d
usuiat <stripewani@gmail.com>
2021-05-05 18:03:05 +0900

    launchで直接スレッドを指定する。

--- app/src/main/java/net/engawapg/app/photogallery/gallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/net/engawapg/app/photogallery/gallery/PhotoGalleryViewModel.kt
@@ -10,7 +10,6 @@ import androidx.lifecycle.viewModelScope
 import androidx.recyclerview.widget.DiffUtil
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import net.engawapg.app.util.Event
 
 /* PhotoGalleryを構成するアイテム */
@@ -46,22 +45,14 @@ class PhotoGalleryViewModel(app: Application) : AndroidViewModel(app) {
 
     /* 写真のリストを読み込む */
     fun loadPhotoList() {
-        /* Scopeを使ってバックグラウンドで動作させる */
-        viewModelScope.launch {
-            photoList.value = queryPhoto()
-        }
-    }
-
-    /* バックグラウンドでMediaStoreから写真を検索する */
-    private suspend fun queryPhoto() : List<PhotoGalleryItem> {
-        /* IO用のスレッドで動作させる */
-        return withContext(Dispatchers.IO) {
+        /* IOスレッドでMediaStoreから写真を検索する */
+        viewModelScope.launch(Dispatchers.IO) {
             val list = mutableListOf<PhotoGalleryItem>()
 
             /* 読み込む列の指定 */
             val projection = arrayOf(
-                    MediaStore.Images.Media._ID, /* ID : URI取得に必要 */
-                    MediaStore.Images.Media.DATE_TAKEN  /* 撮影日時 */
+                MediaStore.Images.Media._ID, /* ID : URI取得に必要 */
+                MediaStore.Images.Media.DATE_TAKEN  /* 撮影日時 */
             )
             val selection = null /* 行の絞り込みの指定。nullならすべての行を読み込む。*/
             val selectionArgs = null /* selectionの?を置き換える引数 */
@@ -69,8 +60,8 @@ class PhotoGalleryViewModel(app: Application) : AndroidViewModel(app) {
             val sortOrder = "${MediaStore.Images.Media.DATE_TAKEN} DESC"
 
             getApplication<Application>().contentResolver.query(
-                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                    projection, selection, selectionArgs, sortOrder
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                projection, selection, selectionArgs, sortOrder
             )?.use { cursor -> /* cursorは、検索結果の各行の情報にアクセスするためのオブジェクト。*/
                 /* 必要な情報が格納されている列番号を取得する。 */
                 val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
@@ -78,12 +69,13 @@ class PhotoGalleryViewModel(app: Application) : AndroidViewModel(app) {
                     val id = cursor.getLong(idColumn)
                     /* IDからURIを取得してリストに格納 */
                     val uri = ContentUris.withAppendedId(
-                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
                     list.add(PhotoGalleryItem(uri))
                 }
             }
 
-            list
+            /* メインスレッドでphotoListにセットする */
+            photoList.postValue(list)
         }
     }
 
